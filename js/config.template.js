9bd1ff539de961acba3f87f96bc2971ce6138699
neha-coditation <neha@coditation.com>
2026-08-26 15:57:49 +0530

    Update config.template.js

--- js/config.template.js
+++ js/config.template.js
@@ -1,7 +1,11 @@
-// Generated during deployment. Do not commit js/config.js.
+// Generated during GitHub Actions deployment. Do not commit js/config.js.
 window.CONTENTFUL_CONFIG = {
   spaceId: "__CONTENTFUL_SPACE_ID__",
-  deliveryToken: "__CONTENTFUL_DELIVERY_TOKEN__"
+  deliveryToken: "__CONTENTFUL_DELIVERY_TOKEN__",
+  environment: "master",
+  contentType: "episode",
+  guestContentType: "guest",
+  enabled: true
 };
 
 window.WEB3FORMS_CONFIG = {
